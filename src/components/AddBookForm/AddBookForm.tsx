12e5a18da48e7affe77d7f72502e960b4a2fbb66
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-07-25 18:34:25 +0200

    ex3.4.

--- src/components/AddBookForm/AddBookForm.tsx
+++ src/components/AddBookForm/AddBookForm.tsx
@@ -1,27 +1,24 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Book } from '../../interfaces/Book.interface';
 import { randomID } from '../../utils/randomId';
+import { addBook as addBookAction } from './../../redux/booksRedux';
+
 import './AddBookForm.css';
 
-interface Props {
-    addBook: (book: Book) => void
-}
+const AddBookForm: React.FC = () => {
+    const dispatch = useDispatch();
+    const addBook = (book: Book) => dispatch(addBookAction(book));
 
-
-
-const AddBookForm: React.FC<Props> = ({ addBook }) => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [price, setPrice] = useState(0);
-
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-
         // const { target } = e;
         // const { value } = target;
         // const { name } = target;
         // setBook([name]: value)
-
         const book: Book = {
             id: randomID(10),
             title,
@@ -39,5 +36,4 @@ const AddBookForm: React.FC<Props> = ({ addBook }) => {
         </form>
     );
 }
-
 export default AddBookForm;
